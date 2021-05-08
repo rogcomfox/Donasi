d2217a0f014eb360982dc358173a341b5f3c0a2c
rogcomfox <joeg.11021996@gmail.com>
2021-05-08 16:15:37 +0700

    add splash page

--- app/src/main/java/com/nusantarian/donasi/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/nusantarian/donasi/ui/activity/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.nusantarian.donasi.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.nusantarian.donasi.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -12,5 +16,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val user = FirebaseAuth.getInstance().currentUser
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (user != null)
+                if (user.uid == "5JpWS9ze6tSlPvSjHj8njYFws533")
+                    startActivity(Intent(this, AdminMainActivity::class.java))
+                else
+                    startActivity(Intent(this, MainActivity::class.java))
+            else
+                startActivity(Intent(this, LandingActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
+
     }
 }
