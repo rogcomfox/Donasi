8a163b64cf705d8c39cd38bf8ff2513dbd23ab0b
rogcomfox <joeg.11021996@gmail.com>
2021-05-10 07:45:34 +0700

    success add page to reset password (Yusuf)

--- app/src/main/java/com/nusantarian/donasi/ui/fragment/landing/ForgotPassFragment.kt
+++ app/src/main/java/com/nusantarian/donasi/ui/fragment/landing/ForgotPassFragment.kt
@@ -4,57 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.nusantarian.donasi.R
+import com.google.firebase.auth.FirebaseAuth
+import com.nusantarian.donasi.databinding.FragmentForgotPassBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ForgotPassFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ForgotPassFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentForgotPassBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_pass, container, false)
+        _binding = FragmentForgotPassBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ForgotPassFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ForgotPassFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSend.setOnClickListener {
+            binding.progress.visibility = View.VISIBLE
+            val email = binding.tilEmail.editText.toString()
+            val auth = FirebaseAuth.getInstance()
+            auth.sendPasswordResetEmail(email).addOnCompleteListener {
+                binding.progress.visibility = View.GONE
+                Toast.makeText(context, "Please Check Your Email Inbox", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                binding.progress.visibility = View.GONE
+                Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
             }
+        }
     }
 }
