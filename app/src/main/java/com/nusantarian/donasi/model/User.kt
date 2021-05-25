0edf202ebeeba098bb73c95ff178fa0b7b89e539
iqbalklv <49069024+iqbalklv@users.noreply.github.com>
2021-05-26 03:47:48 +0700

    Added converter document to user to User.kt
    fungsi docToUser untuk ngubah document dari firestore ke bentuk class User()

--- app/src/main/java/com/nusantarian/donasi/model/User.kt
+++ app/src/main/java/com/nusantarian/donasi/model/User.kt
@@ -1,8 +1,33 @@
 package com.nusantarian.donasi.model
 
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.QueryDocumentSnapshot
+
 data class User(
     val email: String,
     val name: String,
     val phone: String,
     val profileURL: String
-)
+){
+    companion object{
+        fun docToUser(document: QueryDocumentSnapshot): User{
+
+            return User(
+                document["email"].toString(),
+                document["name"].toString(),
+                document["phone"].toString(),
+                document["profileURL"].toString()
+            )
+        }
+
+        fun docToUser(document: DocumentSnapshot): User{
+
+            return User(
+                document["email"].toString(),
+                document["name"].toString(),
+                document["phone"].toString(),
+                document["profileURL"].toString()
+            )
+        }
+    }
+}
