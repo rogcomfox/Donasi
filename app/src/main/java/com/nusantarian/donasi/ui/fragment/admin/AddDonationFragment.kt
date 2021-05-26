b19eb445d9b611a4675b32d49544c799f2b4cc55
rogcomfox <joeg.11021996@gmail.com>
2021-05-26 11:47:56 +0700

    success add donation from admin (Yusuf)

--- app/src/main/java/com/nusantarian/donasi/ui/fragment/admin/AddDonationFragment.kt
+++ app/src/main/java/com/nusantarian/donasi/ui/fragment/admin/AddDonationFragment.kt
@@ -1,60 +1,166 @@
 package com.nusantarian.donasi.ui.fragment.admin
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.firebase.firestore.FirebaseFirestore
 import com.nusantarian.donasi.R
+import com.nusantarian.donasi.databinding.FragmentAddDonationBinding
+import com.nusantarian.donasi.model.Donation
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddDonationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddDonationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentAddDonationBinding? = null
+    private val binding get() = _binding!!
+    private val calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_donation, container, false)
+        _binding = FragmentAddDonationBinding.inflate(inflater, container, false)
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+        setHasOptionsMenu(true)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddDonationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddDonationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home ->
+                requireActivity().onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.etStartDate.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                { _, year, month, day ->
+                    binding.etStartDate.setText(
+                        resources.getString(
+                            R.string.text_date,
+                            year,
+                            month,
+                            day
+                        )
+                    )
+                },
+                calendar[Calendar.YEAR],
+                calendar[Calendar.MONTH],
+                calendar[Calendar.DAY_OF_MONTH]
+            ).show()
+        }
+        binding.etEndDate.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                { _, year, month, day ->
+                    binding.etEndDate.setText(
+                        resources.getString(
+                            R.string.text_date,
+                            year,
+                            month,
+                            day
+                        )
+                    )
+                },
+                calendar[Calendar.YEAR],
+                calendar[Calendar.MONTH],
+                calendar[Calendar.DAY_OF_MONTH]
+            ).show()
+        }
+        binding.btnStartFund.setOnClickListener {
+            binding.progress.visibility = View.VISIBLE
+            val title = binding.tilTitle.editText?.text.toString()
+            val start = binding.tilStartDate.editText?.text.toString()
+            val end = binding.tilEndDate.editText?.text.toString()
+            val target = binding.tilTarget.editText?.text.toString()
+            val story = binding.tilStory.editText?.text.toString()
+
+            if (!isValidInput(title, start, end, target, story))
+                binding.progress.visibility = View.GONE
+            else
+                createDonation(title, start, end, target, story)
+        }
+    }
+
+    private fun createDonation(
+        title: String,
+        start: String,
+        end: String,
+        target: String,
+        story: String
+    ) {
+        val db = FirebaseFirestore.getInstance().collection("donations").document()
+        val model = Donation(title, story, start, end, 0, target.toInt(), 0)
+        db.set(model).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Success Create Donation", Toast.LENGTH_SHORT).show()
+                requireActivity().onBackPressed()
+            } else {
+                Toast.makeText(context, "Failed Create Donation", Toast.LENGTH_SHORT).show()
             }
+            binding.progress.visibility = View.GONE
+        }.addOnFailureListener {
+            Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+            binding.progress.visibility = View.GONE
+        }
+    }
+
+    private fun isValidInput(
+        title: String,
+        start: String,
+        end: String,
+        target: String,
+        story: String
+    ): Boolean {
+        val empty = resources.getString(R.string.text_empty)
+        return when {
+            title.isEmpty() -> {
+                binding.tilTitle.error = empty
+                false
+            }
+            start.isEmpty() -> {
+                binding.tilStartDate.error = empty
+                false
+            }
+            end.isEmpty() -> {
+                binding.tilEndDate.error = empty
+                false
+            }
+            target.isEmpty() -> {
+                binding.tilTarget.error = empty
+                false
+            }
+            story.isEmpty() -> {
+                binding.tilStory.error = empty
+                false
+            }
+            else -> {
+                binding.tilTitle.error = null
+                binding.tilStartDate.error = null
+                binding.tilEndDate.error = null
+                binding.tilTarget.error = null
+                binding.tilStory.error = null
+
+                binding.tilTitle.isErrorEnabled
+                binding.tilTitle.isErrorEnabled
+                binding.tilTitle.isErrorEnabled
+                binding.tilTitle.isErrorEnabled
+                binding.tilTitle.isErrorEnabled
+                true
+            }
+        }
     }
 }
