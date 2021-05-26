6fac43aeafbf03d8bd917a9f0d5f98dc162e13a7
rogcomfox <joeg.11021996@gmail.com>
2021-05-26 10:22:48 +0700

    fix checking error inside email address field (Yusuf)

--- app/src/main/java/com/nusantarian/donasi/ui/fragment/landing/ForgotPassFragment.kt
+++ app/src/main/java/com/nusantarian/donasi/ui/fragment/landing/ForgotPassFragment.kt
@@ -1,12 +1,14 @@
 package com.nusantarian.donasi.ui.fragment.landing
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
+import com.nusantarian.donasi.R
 import com.nusantarian.donasi.databinding.FragmentForgotPassBinding
 
 class ForgotPassFragment : Fragment() {
@@ -28,13 +30,23 @@ class ForgotPassFragment : Fragment() {
         binding.btnSend.setOnClickListener {
             binding.progress.visibility = View.VISIBLE
             val email = binding.tilEmail.editText.toString()
-            val auth = FirebaseAuth.getInstance()
-            auth.sendPasswordResetEmail(email).addOnCompleteListener {
+
+            if (email.isEmpty()) {
                 binding.progress.visibility = View.GONE
-                Toast.makeText(context, "Please Check Your Email Inbox", Toast.LENGTH_SHORT).show()
-            }.addOnFailureListener {
+                binding.tilEmail.error = resources.getString(R.string.text_empty)
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 binding.progress.visibility = View.GONE
-                Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+                binding.tilEmail.error = resources.getString(R.string.text_invalid)
+            } else {
+                val auth = FirebaseAuth.getInstance()
+                auth.sendPasswordResetEmail(email).addOnCompleteListener {
+                    binding.progress.visibility = View.GONE
+                    Toast.makeText(context, "Please Check Your Email Inbox", Toast.LENGTH_SHORT)
+                        .show()
+                }.addOnFailureListener {
+                    binding.progress.visibility = View.GONE
+                    Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
